b75afdf346183bb311bcc1ed318db6cd268bf51f
Rakka Toar <rakkatoar@gmail.com>
2022-04-10 00:42:11 +0700

    fix carousel

--- components/CarouselSection.jsx
+++ components/CarouselSection.jsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 const CarouselSection = () => {
-
+    const [sliders, setSliders] = useState([
+		{ image: "/images/blue-logo.png" },
+		{ image: "/images/blue-logo.png" },
+		{ image: "/images/blue-logo.png" },
+		{ image: "/images/blue-logo.png" },
+	]);
     return (
 				<div className="carousel-wrapper w-full p-16 bg-grey">
                     <style jsx>
@@ -23,18 +29,13 @@ const CarouselSection = () => {
 							ease: "linear",
 					}}>
                         <Carousel className="h-full" autoPlay showStatus={false} showThumbs={false} showArrows={false} dynamicHeight={false}>
-                            <div className="h-[40vh] bg-dark-grey flex justify-center items-center rounded-lg">
-                                <Image src="/images/blue-logo.png" width="180" height="120" alt="image1"/>
-                            </div>
-                            <div className="h-[40vh] bg-dark-grey flex justify-center items-center rounded-lg">
-                                <Image src="/images/blue-logo.png" width="180" height="120" alt="image2" />
-                            </div>
-                            <div className="h-[40vh] bg-dark-grey flex justify-center items-center rounded-lg">
-                                <Image src="/images/blue-logo.png" width="180" height="120" alt="image3"/>
-                            </div>
-                            <div className="h-[40vh] bg-dark-grey flex justify-center items-center rounded-lg">
-                                <Image src="/images/blue-logo.png" width="180" height="120" alt="image4"/>
-                            </div>
+                            {
+                                sliders.map((media, i) => (
+                                    <div className="h-[40vh] bg-dark-grey flex justify-center items-center rounded-lg">
+                                        <Image src={media.image} width="180" height="120" alt={"image"+i+1}/>
+                                    </div>
+                                ))
+                            }
                         </Carousel>
                     </motion.div>
 				</div>
